29c036510bd78c6d44ce178a72492c6cfa0d54f7
Uzair-DeVops <uzairyasin83@gmail.com>
2026-06-03 19:53:11 +0500

    error solved

--- install.js
+++ install.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Get the project root (where npm install was run)
 const projectRoot = process.cwd();
